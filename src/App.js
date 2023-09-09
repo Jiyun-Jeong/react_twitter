717fd7f271d9cc4ab98834db27746c6b55bc0d2c
Jiyun-Jeong <rijamong0830@naver.com>
2023-09-09 14:40:20 +0900

    mode 지정

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import TwitFeed from "./TwitFeed";
 //https://jsonplaceholder.typicode.com/comments
 
 function App() {
-  const [data, setData] = useState([]);
-  //const [mode, setMode] = useState("list");
+  const [data, setData] = useState([]); // 트윗 목록 저장
+  const [mode, setMode] = useState("list"); // write | list 모드에 따라 내용이 바뀜
 
   const dataId = useRef(0);
 
@@ -22,10 +22,12 @@ function App() {
     return randomTxt[Math.floor(Math.random() * randomTxt.length)];
   };
 
+  //api 10개만 사용
   const getData = async () => {
     const res = await fetch(
       "https://jsonplaceholder.typicode.com/comments"
     ).then((res) => res.json());
+
     console.log(res);
 
     const initData = res.slice(0, 10).map((it) => {
@@ -39,15 +41,6 @@ function App() {
         image: "https://cataas.com/cat/zPHzhfP1n01EMfKy",
       };
     });
-
-    // const thumbData = response.slice(0, 10).map((it) => {
-    //   const imgUrl = 'https://cataas.com' + it.url;
-
-    //   return {
-    //     image:imgUrl
-    //   };
-    // });
-
     setData(initData);
   };
 
@@ -55,45 +48,30 @@ function App() {
     getData();
   }, []);
 
-  const onCreate = (author, content, image) => {
-    const newCat = async () => {
-      const catApiUrl = "https://cataas.com";
-      const catList = await fetch(catApiUrl + "/cat?json=true").then(
-        (response) => response.json()
-      );
-      const catUrl = catApiUrl + catList.url;
+  const onCreate = async (author, content, image) => {
+    const catApiUrl = "https://cataas.com";
+    const catList = await fetch(catApiUrl + "/cat?json=true")
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        const catUrl = catApiUrl + response.url;
 
-      return catUrl;
-    };
-
-    console.log(newCat().then((e) => console.log(e)));
-
-    const test = newCat();
-
-    const created_date = new Date().getTime();
-    const newItem = {
-      author,
-      created_date,
-      id: dataId.current,
-      image: newCat().then((e) => console.log(e)),
-    };
-    dataId.current += 1;
-    setData([newItem, ...data]);
+        const created_date = new Date().getTime();
+        const newItem = {
+          author,
+          created_date,
+          id: dataId.current,
+          image: catUrl,
+        };
+        dataId.current += 1;
+        setData([newItem, ...data]);
+      });
   };
 
   const onRemove = (targetId) => {
     const newTwitList = data.filter((it) => it.id !== targetId);
     setData(newTwitList);
   };
-
-  // useEffect(() => {
-  //   axios.get("https://cataas.com/cat").then((res) => {
-  //     setData(res.data.results.map(user => ({
-  //       content: randomTwit(),
-  //       image: user.picture.thumbnail
-  //     })))
-  //   })
-  // }, [])
 
   return (
     <div className="app">
@@ -104,8 +82,8 @@ function App() {
       </header>
       <section>
         <div className="container">
-          <TwitWrite onCreate={onCreate} />
           <TwitFeed onRemove={onRemove} twitList={data} />
+          {mode === "write" ? <TwitWrite onCreate={onCreate} /> : ""}
           <button className="twit">
             <span className="hidden">트윗하기</span>
           </button>

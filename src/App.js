8dfbb903bdc2de57334f9d9001d88f5c8c36ac96
Jiyun-Jeong <rijamong0830@naver.com>
2023-09-09 13:16:17 +0900

    html set

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import TwitFeed from "./TwitFeed";
 
 //https://jsonplaceholder.typicode.com/comments
 
-
-
 function App() {
   const [data, setData] = useState([]);
   //const [mode, setMode] = useState("list");
@@ -15,60 +13,69 @@ function App() {
   const dataId = useRef(0);
 
   const randomTwit = () => {
-    const randomTxt = ["오늘 무두일이래", "무두일이 대체 뭔데", "없을무 우두머리두 날일 써서 우두머리가 없는날....", "개이득?"]
-    return randomTxt[Math.floor(Math.random() * randomTxt.length)]
-  }
+    const randomTxt = [
+      "오늘 무두일이래",
+      "무두일이 대체 뭔데",
+      "없을무 우두머리두 날일 써서 우두머리가 없는날....",
+      "개이득?",
+    ];
+    return randomTxt[Math.floor(Math.random() * randomTxt.length)];
+  };
 
   const getData = async () => {
-    const res = await fetch('https://jsonplaceholder.typicode.com/comments').then((res) => res.json());
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    ).then((res) => res.json());
+    console.log(res);
 
-    const initData = res.slice(0, 1).map((it) => {
-      const emailArray = it.email.split('@');
+    const initData = res.slice(0, 10).map((it) => {
+      const emailArray = it.email.split("@");
 
       return {
         author: emailArray[0],
         content: randomTwit(),
         //time: new Date().getTime(),
         id: dataId.current++,
-        image: 'https://cataas.com/cat/zPHzhfP1n01EMfKy'
+        image: "https://cataas.com/cat/zPHzhfP1n01EMfKy",
       };
     });
 
     // const thumbData = response.slice(0, 10).map((it) => {
     //   const imgUrl = 'https://cataas.com' + it.url;
-      
+
     //   return {
     //     image:imgUrl
     //   };
     // });
 
     setData(initData);
-  }
+  };
 
   useEffect(() => {
     getData();
-  },[])
+  }, []);
 
   const onCreate = (author, content, image) => {
-
     const newCat = async () => {
-      const catApiUrl = 'https://cataas.com';
-      const catList = await fetch(catApiUrl + '/cat?json=true').then((response) => response.json());
+      const catApiUrl = "https://cataas.com";
+      const catList = await fetch(catApiUrl + "/cat?json=true").then(
+        (response) => response.json()
+      );
       const catUrl = catApiUrl + catList.url;
 
       return catUrl;
-    }
+    };
 
     console.log(newCat().then((e) => console.log(e)));
-    
+
     const test = newCat();
-    
+
     const created_date = new Date().getTime();
     const newItem = {
       author,
       created_date,
       id: dataId.current,
-      image: newCat().then((e) => console.log(e))
+      image: newCat().then((e) => console.log(e)),
     };
     dataId.current += 1;
     setData([newItem, ...data]);
